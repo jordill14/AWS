f69df0275c15ee46fb8990f7ee9e60ff3a3ec7d8
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-11-30 10:29:10 +1100

    Add "SKIP" DynamoDB create mode back

--- dynamodb-java/src/main/java/org/paradise/microservice/userpreference/service/dynamodb/DynamoDbTableCreateMode.java
+++ dynamodb-java/src/main/java/org/paradise/microservice/userpreference/service/dynamodb/DynamoDbTableCreateMode.java
@@ -3,5 +3,6 @@ package org.paradise.microservice.userpreference.service.dynamodb;
 public enum DynamoDBTableCreateMode {
 
     DROP, // drops tables on very restart
+    SKIP  // creates tables only if they do not exist
 
 }
