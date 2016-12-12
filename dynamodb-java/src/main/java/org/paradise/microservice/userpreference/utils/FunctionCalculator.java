1a8fc09e5486fb0c870d9e00468edc7802b4980e
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-12-12 12:45:04 +1100

    Add Factorial function and unit test

--- dynamodb-java/src/main/java/org/paradise/microservice/userpreference/utils/FunctionCalculator.java
+++ dynamodb-java/src/main/java/org/paradise/microservice/userpreference/utils/FunctionCalculator.java
@@ -12,12 +12,22 @@ public class FunctionCalculator {
     private static Map<Integer, Long> fibonacciMap = new HashMap<>();
 
     static {
-        fibonacciMap.put(0, 0L); //fibonacci(0)
-        fibonacciMap.put(1, 1L); //fibonacci(1)
+        fibonacciMap.put(0, 0L); // fibonacci(0)
+        fibonacciMap.put(1, 1L); // fibonacci(1)
     }
 
     public static Function<Integer, Long> fibonacci = (x) ->
             fibonacciMap.computeIfAbsent(x,
                     n -> FunctionCalculator.fibonacci.apply(n - 2) + FunctionCalculator.fibonacci.apply(n - 1));
+
+    private static Map<Integer, Long> factorialMap = new HashMap<>();
+
+    static {
+        factorialMap.put(1, 1L); // factorial(1)
+    }
+
+    public static Function<Integer, Long> factorial = (x) ->
+            factorialMap.computeIfAbsent(x,
+                    n -> n * FunctionCalculator.factorial.apply(n - 1));
 
 }
--- dynamodb-java/src/test/java/org/paradise/microservice/userpreference/utils/FunctionCalculatorTest.java
+++ dynamodb-java/src/test/java/org/paradise/microservice/userpreference/utils/FunctionCalculatorTest.java
@@ -12,18 +12,29 @@ public class FunctionCalculatorTest {
     @Test
     public void testFibonacciFunction() throws Exception {
 
-        FunctionCalculator functionCalculator = new FunctionCalculator();
+        assertEquals("Incorrect result", Long.valueOf(0), FunctionCalculator.fibonacci.apply(0));
+        assertEquals("Incorrect result", Long.valueOf(1), FunctionCalculator.fibonacci.apply(1));
+        assertEquals("Incorrect result", Long.valueOf(1), FunctionCalculator.fibonacci.apply(2));
+        assertEquals("Incorrect result", Long.valueOf(2), FunctionCalculator.fibonacci.apply(3));
+        assertEquals("Incorrect result", Long.valueOf(3), FunctionCalculator.fibonacci.apply(4));
+        assertEquals("Incorrect result", Long.valueOf(5), FunctionCalculator.fibonacci.apply(5));
+        assertEquals("Incorrect result", Long.valueOf(8), FunctionCalculator.fibonacci.apply(6));
 
-        assertEquals("Incorrect result", Long.valueOf(1),  functionCalculator.fibonacci.apply(2));
-        assertEquals("Incorrect result", Long.valueOf(2),  functionCalculator.fibonacci.apply(3));
-        assertEquals("Incorrect result", Long.valueOf(3),  functionCalculator.fibonacci.apply(4));
-        assertEquals("Incorrect result", Long.valueOf(5),  functionCalculator.fibonacci.apply(5));
-        assertEquals("Incorrect result", Long.valueOf(8),  functionCalculator.fibonacci.apply(6));
-        assertEquals("Incorrect result", Long.valueOf(13), functionCalculator.fibonacci.apply(7));
-        assertEquals("Incorrect result", Long.valueOf(21), functionCalculator.fibonacci.apply(8));
-        assertEquals("Incorrect result", Long.valueOf(34), functionCalculator.fibonacci.apply(9));
+        assertEquals("Incorrect result", Long.valueOf(13), FunctionCalculator.fibonacci.apply(7));
+        assertEquals("Incorrect result", Long.valueOf(21), FunctionCalculator.fibonacci.apply(8));
+        assertEquals("Incorrect result", Long.valueOf(34), FunctionCalculator.fibonacci.apply(9));
+        assertEquals("Incorrect result", Long.valueOf(55), FunctionCalculator.fibonacci.apply(10));
 
-        assertEquals("Incorrect result", Long.valueOf(12586269025L), functionCalculator.fibonacci.apply(50));
+        assertEquals("Incorrect result", Long.valueOf(12586269025L), FunctionCalculator.fibonacci.apply(50));
+    }
+
+    @Test
+    public void testFactorialFunction() throws Exception {
+
+        assertEquals("Incorrect result", Long.valueOf(1), FunctionCalculator.factorial.apply(1));
+        assertEquals("Incorrect result", Long.valueOf(2), FunctionCalculator.factorial.apply(2));
+
+        assertEquals("Incorrect result", Long.valueOf(3628800), FunctionCalculator.factorial.apply(10));
     }
 
 }
