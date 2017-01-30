604d44b1c571c55817d0d4a30284c341b7e76f8b
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-01-31 09:50:41 +1100

    Add Error Handler for RestTemplate and refactor code

--- dynamodb-java/src/main/java/org/paradise/microservice/userpreference/service/rest/RestServiceClient.java
+++ dynamodb-java/src/main/java/org/paradise/microservice/userpreference/service/rest/RestServiceClient.java
@@ -5,14 +5,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,13 +33,8 @@ public class RestServiceClient {
         restTemplate = new RestTemplate();
 
         restTemplate.setRequestFactory(httpRequestInterceptor);
-
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        messageConverters.add(new FormHttpMessageConverter());
-        messageConverters.add(new StringHttpMessageConverter());
-        messageConverters.add(new MappingJackson2HttpMessageConverter());
-
-        restTemplate.setMessageConverters(messageConverters);
+        restTemplate.setMessageConverters(createHttpMessageConverter());
+        restTemplate.setErrorHandler(createResponseErrorHandler());
     }
 
     public <T> ResponseEntity<T> exchange(String url, HttpMethod method,
@@ -45,5 +43,43 @@ public class RestServiceClient {
 
         return restTemplate.exchange(url, method, requestEntity, responseType, uriVariables);
     }
-    
+
+    /**
+     * Create default HTTP Request converter list.
+     *
+     * @return HttpMessageConverter created
+     */
+    private List<HttpMessageConverter<?>> createHttpMessageConverter() {
+
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+
+        messageConverters.add(new FormHttpMessageConverter());
+        messageConverters.add(new StringHttpMessageConverter());
+        messageConverters.add(new MappingJackson2HttpMessageConverter());
+
+        return messageConverters;
+    }
+
+    /**
+     * By pass the default Exception Handler. When make RESTful call, disable any error handler when use this
+     * restTemplate, and directly back error message to service(s) or controller(s).
+     *
+     * @return ResponseErrorHandler created
+     */
+    private ResponseErrorHandler createResponseErrorHandler() {
+
+        return new ResponseErrorHandler() {
+
+            @Override
+            public boolean hasError(ClientHttpResponse response) throws IOException {
+                return false;
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+
+            }
+        };
+    }
+
 }
