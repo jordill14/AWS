8a0d1305a863d542fd09e50aabcae3e11eef1f80
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-03-17 16:09:06 +1100

    Add more TimeUtil unit test

--- dynamodb-java/src/test/java/org/paradise/microservice/userpreference/util/TimeUtilTest.java
+++ dynamodb-java/src/test/java/org/paradise/microservice/userpreference/util/TimeUtilTest.java
@@ -2,11 +2,14 @@ package org.paradise.microservice.userpreference.util;
 
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -45,6 +48,42 @@ public class TimeUtilTest {
 
         assertEquals("Offset in Millisecond is incorrect",
                 Long.valueOf(60000), TimeUtil.betweenInMillisecond(now, now.plusMinutes(1)));
+    }
+
+    @Test
+    public void testDateTimeParseWithISODateTime() {
+
+        String localDateTime = "2017-03-16T23:42:42.691";
+        String utcDateTime = "2017-03-16T23:42:42.691Z";
+        String systemDefaultDateTime = "2017-03-16T23:42:42.691+11:00";
+        String melbourneDateTime = "2017-03-16T23:42:42.691+11:00[Australia/Melbourne]";
+
+        LocalDateTime localLocalDateTime = LocalDateTime.parse(localDateTime, DateTimeFormatter.ISO_DATE_TIME);
+        LocalDateTime utcLocalDateTime = LocalDateTime.parse(utcDateTime, DateTimeFormatter.ISO_DATE_TIME);
+        LocalDateTime systemDefaultLocalDateTime = LocalDateTime.parse(systemDefaultDateTime, DateTimeFormatter.ISO_DATE_TIME);
+        LocalDateTime melbourneLocalDateTime = LocalDateTime.parse(melbourneDateTime, DateTimeFormatter.ISO_DATE_TIME);
+
+        ZonedDateTime localZonedDateTime = ZonedDateTime.parse(localDateTime + "Z");
+        ZonedDateTime utcZonedDateTime = ZonedDateTime.parse(utcDateTime);
+        ZonedDateTime systemDefaultZonedDateTime = ZonedDateTime.parse(systemDefaultDateTime);
+        ZonedDateTime melbourneZonedDateTime = ZonedDateTime.parse(melbourneDateTime);
+
+        assertEquals("Incorrect conversion", localLocalDateTime, localZonedDateTime.toLocalDateTime());
+        assertEquals("Incorrect conversion", utcLocalDateTime, utcZonedDateTime.toLocalDateTime());
+        assertEquals("Incorrect conversion", systemDefaultLocalDateTime, systemDefaultZonedDateTime.toLocalDateTime());
+        assertEquals("Incorrect conversion", melbourneLocalDateTime, melbourneZonedDateTime.toLocalDateTime());
+    }
+
+    @Test
+    public void test2() {
+
+        LocalDateTime nowLocalDateTime = LocalDateTime.now();
+        ZonedDateTime nowZonedDateTime = ZonedDateTime.now();
+
+        String nowLocalDateTimeISO = nowLocalDateTime.toString() + "Z";
+        String nowZonedDateTimeISO = nowZonedDateTime.format(DateTimeFormatter.ISO_INSTANT);
+
+        assertNotEquals("Incorrect conversion", nowZonedDateTimeISO, nowLocalDateTimeISO);
     }
 
 }
