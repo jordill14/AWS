aabab181aaef2aa1b9a49e4da7f6aa2ed4264cef
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-04-04 22:53:12 +1000

    Refactor CSVFileWriter test to integration test

--- dynamodb-java/src/test/java/org/paradise/microservice/userpreference/service/writer/CSVFileWriterIntegrationTest.java
+++ dynamodb-java/src/test/java/org/paradise/microservice/userpreference/service/writer/CSVFileWriterIntegrationTest.java
@@ -1,19 +1,23 @@
-package org.paradise.microservice.userpreference.functional;
+package org.paradise.microservice.userpreference.service.writer;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
 import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.WritableResource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
@@ -21,21 +25,25 @@ import static org.mockito.Mockito.when;
 /**
  * Created by terrence on 29/3/17.
  */
-@DirtiesContext
-@TestPropertySource("/test.properties")
-@Ignore
-public class CSVFileWriterFunctionalTest extends AbstractFunctionalTest {
+@RunWith(SpringRunner.class)
+public class CSVFileWriterIntegrationTest {
 
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    @MockBean(name = "amazonS3Client")
-    private AmazonS3 amazonS3;
+    @MockBean
+    private AmazonS3 mockAmazonS3;
 
-    private File importFile, statusFile, errorFile;
+    private File errorFile;
+    private WritableResource errorResource;
+
+    private CSVFileWriter csvFileWriter = new CSVFileWriter();
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
+
+        errorFile = temporaryFolder.newFile("error.csv");
+        errorResource = new FileSystemResource(errorFile);
 
         Answer<PutObjectResult> answer = invocation -> {
             PutObjectRequest putObjectRequest = invocation.getArgumentAt(0, PutObjectRequest.class);
@@ -47,15 +55,15 @@ public class CSVFileWriterFunctionalTest extends AbstractFunctionalTest {
             return new PutObjectResult();
         };
 
-        when(amazonS3.putObject(any(PutObjectRequest.class))).thenAnswer(answer);
+        when(mockAmazonS3.putObject(any(PutObjectRequest.class))).thenAnswer(answer);
+
+        ReflectionTestUtils.setField(csvFileWriter, "amazonS3", mockAmazonS3);
     }
 
     @Test
     public void write() throws Exception {
 
-        importFile = temporaryFolder.newFile("import.csv");
-        statusFile = temporaryFolder.newFile("status.json");
-        errorFile = temporaryFolder.newFile("error.csv");
+        csvFileWriter.write(errorResource, new ArrayList());
     }
 
     @Test
