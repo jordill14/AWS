751d7363c4fe175e311cd13978c0b3e9d5e81849
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-05-09 20:01:35 +1000

    Add unit test for Spring MVC controller

---
+++ dynamodb-java/src/test/java/org/paradise/microservice/userpreference/controller/UserPreferenceControllerTest.java
@@ -0,0 +1,32 @@
+package org.paradise.microservice.userpreference.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by terrence on 9/5/17.
+ */
+public class UserPreferenceControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getUserPreferencesList() throws Exception {
+    }
+
+    @Test
+    public void getUserPreferences() throws Exception {
+    }
+
+    @Test
+    public void createUserPreferences() throws Exception {
+    }
+
+}
