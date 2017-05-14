bb5051feca62c968abcc9dc95ae3ebb5f4aca0c6
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-05-14 13:31:20 +1000

    Add Taylor Series for e (Euler’s number) in Function Calculator

--- dynamodb-java/src/main/java/org/paradise/microservice/userpreference/util/FunctionCalculator.java
+++ dynamodb-java/src/main/java/org/paradise/microservice/userpreference/util/FunctionCalculator.java
@@ -9,17 +9,26 @@ import java.util.function.Function;
  */
 public final class FunctionCalculator {
 
+    // Fibonacci formula
     public static final Map<Integer, Long> FIBONACCI_MAP = new HashMap<>();
 
     public static final Function<Integer, Long> FIBONACCI = (x) ->
             FIBONACCI_MAP.computeIfAbsent(x,
                     n -> FunctionCalculator.FIBONACCI.apply(n - 2) + FunctionCalculator.FIBONACCI.apply(n - 1));
 
+    // Factorial formula
     public static final Map<Integer, Long> FACTORIAL_MAP = new HashMap<>();
 
     public static final Function<Integer, Long> FACTORIAL = (x) ->
             FACTORIAL_MAP.computeIfAbsent(x,
                     n -> n * FunctionCalculator.FACTORIAL.apply(n - 1));
+
+    // Taylor Series for e (Euler's number)
+    public static final Map<Integer, Long> F_MAP = new HashMap<>();
+
+    public static final Function<Integer, Long> F = (x) ->
+            F_MAP.computeIfAbsent(x,
+                    n -> (n - 1) * (FunctionCalculator.F.apply(n - 1) + FunctionCalculator.F.apply(n - 2)));
 
     static {
         FIBONACCI_MAP.put(0, 0L); // FIBONACCI(0)
@@ -30,7 +39,13 @@ public final class FunctionCalculator {
         FACTORIAL_MAP.put(1, 1L); // FACTORIAL(1)
     }
 
+    static {
+        F_MAP.put(1, 0L); // F(1)
+        F_MAP.put(2, 1L); // F(2)
+    }
+
     private FunctionCalculator() {
+
     }
 
 }
--- dynamodb-java/src/test/java/org/paradise/microservice/userpreference/util/FunctionCalculatorTest.java
+++ dynamodb-java/src/test/java/org/paradise/microservice/userpreference/util/FunctionCalculatorTest.java
@@ -1,13 +1,23 @@
 package org.paradise.microservice.userpreference.util;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by terrence on 12/12/2016.
  */
 public class FunctionCalculatorTest {
+
+    public static final Logger LOG = LoggerFactory.getLogger(FunctionCalculatorTest.class);
 
     @Test
     public void testFibonacciFunction() throws Exception {
@@ -31,10 +41,53 @@ public class FunctionCalculatorTest {
     @Test
     public void testFactorialFunction() throws Exception {
 
-        assertEquals("Incorrect result", Long.valueOf(1), FunctionCalculator.FACTORIAL.apply(1));
-        assertEquals("Incorrect result", Long.valueOf(2), FunctionCalculator.FACTORIAL.apply(2));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(1), is(Long.valueOf(1)));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(2), is(Long.valueOf(2)));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(3), is(Long.valueOf(6)));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(4), is(Long.valueOf(24)));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(5), is(Long.valueOf(120)));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(6), is(Long.valueOf(720)));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(7), is(Long.valueOf(5040)));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(8), is(Long.valueOf(40320)));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(9), is(Long.valueOf(362880)));
 
-        assertEquals("Incorrect result", Long.valueOf(3628800), FunctionCalculator.FACTORIAL.apply(10));
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(10), is(Long.valueOf(3628800)));
+
+        assertThat("Incorrect result", FunctionCalculator.FACTORIAL.apply(20), is(Long.valueOf(2432902008176640000L)));
+    }
+
+    @Test
+    public void testFFunction() throws Exception {
+
+        assertThat("Incorrect result", FunctionCalculator.F.apply(1), is(Long.valueOf(0)));
+        assertThat("Incorrect result", FunctionCalculator.F.apply(2), is(Long.valueOf(1)));
+        assertThat("Incorrect result", FunctionCalculator.F.apply(3), is(Long.valueOf(2)));
+        assertThat("Incorrect result", FunctionCalculator.F.apply(4), is(Long.valueOf(9)));
+        assertThat("Incorrect result", FunctionCalculator.F.apply(5), is(Long.valueOf(44)));
+        assertThat("Incorrect result", FunctionCalculator.F.apply(6), is(Long.valueOf(265)));
+        assertThat("Incorrect result", FunctionCalculator.F.apply(7), is(Long.valueOf(1854)));
+        assertThat("Incorrect result", FunctionCalculator.F.apply(8), is(Long.valueOf(14833)));
+        assertThat("Incorrect result", FunctionCalculator.F.apply(9), is(Long.valueOf(133496)));
+
+        assertThat("Incorrect result", FunctionCalculator.F.apply(10), is(Long.valueOf(1334961)));
+
+        assertThat("Incorrect result", FunctionCalculator.F.apply(20), is(Long.valueOf(895014631192902121L)));
+
+        String outputFormat = "%20s lose in total %20s ratio %1.17f / %1.17f for f(%d)";
+
+        IntStream.rangeClosed(1, 20)
+                .forEach(i -> {
+                    long lose = FunctionCalculator.F.apply(i);
+
+                    List<Integer> integerList = new ArrayList<>();
+                    IntStream.rangeClosed(1, i).forEach(j -> integerList.add(j));
+
+                    long total = FunctionCalculator.FACTORIAL.apply(i);
+
+                    LOG.info(String.format(outputFormat, lose, total, 1.0 * lose / total, (1 - 1.0 * lose / total), i));
+                });
+
+        LOG.info(String.format("Taylor Series for e (Euler's number): 1 - 1 / e = %1.17f", 1 - 1.0 / Math.E));
     }
 
 }
