c26d3ca122fb66f85c4d96124dfcc1923d11583e
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2020-02-28 15:34:43 +1100

    Add multiple RDS startup/shutdown support

--- db-startup-shutdown-lambda/shut-down/app.js
+++ db-startup-shutdown-lambda/shut-down/app.js
@@ -1,10 +1,15 @@
 const stopInstance = require('./stop');
 
 exports.lambdaHandler = async (event, context) => {
-    const instanceIdentifier = process.env.INSTANCE_IDENTIFIER;
-    const result = await stopInstance(instanceIdentifier);
-    return {
-        statusCode: 200,
-        body: result,
-    }
+
+  const mysqlInstanceIdentifier = process.env.MYSQL_INSTANCE_IDENTIFIER;
+  const mysqlResult = await stopInstance(mysqlInstanceIdentifier);
+
+  const postgresqlInstanceIdentifier = process.env.POSTGRESQL_INSTANCE_IDENTIFIER;
+  const postgresqlResult = await stopInstance(postgresqlInstanceIdentifier);
+
+  return {
+    statusCode: 200,
+    body: { mysqlResult, postgresqlResult },
+  }
 };
--- db-startup-shutdown-lambda/start-up/app.js
+++ db-startup-shutdown-lambda/start-up/app.js
@@ -1,10 +1,15 @@
 const startInstance = require('./start');
 
 exports.lambdaHandler = async (event, context) => {
-    const instanceIdentifier = process.env.INSTANCE_IDENTIFIER;
-    const result = await startInstance(instanceIdentifier);
-    return {
-        statusCode: 200,
-        body: result,
-    }
+
+  const mysqlInstanceIdentifier = process.env.MYSQL_INSTANCE_IDENTIFIER;
+  const mysqlResult = await startInstance(mysqlInstanceIdentifier);
+
+  const postgresqlInstanceIdentifier = process.env.POSTGRESQL_INSTANCE_IDENTIFIER;
+  const postgresqlResult = await startInstance(postgresqlInstanceIdentifier);
+
+  return {
+    statusCode: 200,
+    body: { mysqlResult, postgresqlResult },
+  }
 };
